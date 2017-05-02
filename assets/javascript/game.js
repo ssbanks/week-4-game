ac921b3206d9f142b057e5383b78d608de5c6783
Your Name <shirlonbanks@Ss-MacBook-Air.local>
2017-05-01 20:32:06 -0700

    Initial files

---
+++ assets/javascript/game.js
@@ -0,0 +1,17 @@
+$( document ).ready(function() {
+
+//Random number from 19 to 120(goal)
+	var goalNumber = Math.floor(Math.random() * 120) + 19;
+		console.log(goalNumber);
+//Random number from 1 to 12 for each crystal(onclick)
+	var crystalNum = Math.floor(Math.random() * 12) + 1;
+		console.log(crystalNum);
+
+//$("<img>").on("click", function(event){
+
+	//add random number as value to crystal
+	//add value to total
+	//compare total to goal number
+	//win
+	//lose
+//})
